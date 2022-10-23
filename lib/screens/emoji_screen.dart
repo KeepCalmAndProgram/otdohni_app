6cacefa736208513d633ca06be41bd8bc5b736db
Andrew Vyshnytskyi <awisznicki2000@gmail.com>
2022-10-23 21:27:06 +0300

    screen styling

--- lib/screens/emoji_screen.dart
+++ lib/screens/emoji_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:otdohni_app/widgets/emoji_animation.dart';
+import 'package:otdohni_app/widgets/relax_phrases_generator/phrase_generator.dart';
 
 class EmojiScreen extends StatefulWidget {
   const EmojiScreen({Key? key}) : super(key: key);
@@ -14,10 +16,38 @@ class _EmojiScreenState extends State<EmojiScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          EmojiAnimation(),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.appbar_home_title),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.assessment_outlined,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.settings,
+            ),
+          ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            EmojiAnimation(),
+            PhraseAutoGenerator(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // there myst be a navigation logic
+        },
+        label: Text('Start The Rest'),
       ),
     );
   }
